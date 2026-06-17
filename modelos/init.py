0a836cc4d317209f1bb45cddcc248dfdcab678b3
Ray <albierisveliz@gmail.com>
2026-06-16 21:49:25 -0500

    Add files via upload

---
+++ modelos/init.py
@@ -0,0 +1,6 @@
+from .estudiante import Estudiante
+from .profesor import Profesor
+from .aula import Aula
+from .curso import Curso
+from .postulante import Postulante
+from .matricula import Matricula
